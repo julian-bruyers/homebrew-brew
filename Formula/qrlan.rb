d79c720f59d767b0247dcb94eb340c8c810c18e2
Julian Bruyers <julian.bruyers@gmail.com>
2025-05-14 02:14:56 +0200

    Update qrlan to v1.1.0

--- Formula/qrlan.rb
+++ Formula/qrlan.rb
@@ -1,10 +1,10 @@
 class Qrlan < Formula
   desc "create wifi qr-codes via the command line (win/mac/linux)"
   homepage "https://github.com/Julian-Bruyers/qrlan-cli"
-  url "initial"
-  sha256 "initial"
+  url "https://github.com/Julian-Bruyers/qrlan-cli/releases/download/14-05-2025-v1.1.0/qrlan-macos-universal.tar.gz"
+  sha256 "bdc65e78ee8c0102dc8bdc6ee3e2225e563f052f78ba3aea46b072cffc0debde"
   license "MIT"
-  version "inital"
+  version "1.1.0"
 
   def install
     bin.install "qrlan"
